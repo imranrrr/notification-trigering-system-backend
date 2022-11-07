c1ac9957b3c755295b3c61e36525c9db2ef4c354
emran <emranlateef786@gmail.com>
2022-11-07 19:38:55 +0500

    fixed upload

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,3 +1,4 @@
 class Template < ApplicationRecord
     belongs_to :user
+    mount_uploader :audio, TemplateAudioUploader
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
   has_many :templates
-  mount_uploader :audio, TemplateAudioUploader
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise  :database_authenticatable, :registerable, :validatable,
