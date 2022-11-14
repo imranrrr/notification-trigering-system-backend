6c19fde7520390b180d406de7bde95f931e9c2a6
emran <emranlateef786@gmail.com>
2022-11-14 19:01:21 +0500

    added destination to endpoints serializers

--- app/serializers/endpoint_serializer.rb
+++ app/serializers/endpoint_serializer.rb
@@ -7,6 +7,14 @@ class EndpointSerializer
   end
 
   attribute :location do |endpoint|
-    {name: endpoint.location.name}
+    if endpoint.location.present?
+      {name: endpoint.location.name}
+    end
   end 
+
+  attribute :destination do |endpoint|
+    if endpoint.destination.present?
+      {destination_type: endpoint.destination.destination_type, resource_url: endpoint.destination.resource_url, network_distribution_id: endpoint.destination.network_distribution_id}
+    end
+  end
 end
