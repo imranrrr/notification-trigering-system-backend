34ddc064e80efdb2c98b00267302372003a42845
emran <emranlateef786@gmail.com>
2022-11-17 18:32:02 +0500

    fixed integration id issue

--- app/serializers/integration_serializer.rb
+++ app/serializers/integration_serializer.rb
@@ -1,6 +1,6 @@
 class IntegrationSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :client_id, :client_secret, :base_url
+  attributes :id, :name, :client_id, :client_secret, :base_url
 
   attribute :created_at do |integration|
     integration.created_at && integration.created_at.strftime('%d/%m/%Y')
