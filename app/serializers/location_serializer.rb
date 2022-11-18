c0bd566149bbbded20eff330b904b6f007f84244
emran <emranlateef786@gmail.com>
2022-11-18 11:45:13 +0500

    fixed location web signage issue

--- app/serializers/location_serializer.rb
+++ app/serializers/location_serializer.rb
@@ -1,6 +1,12 @@
 class LocationSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :name, :web_signage_id, :description, :admin_id
+
+  attribute :web_signage do |location|
+    if location.web_signage.present?
+      {id: location.web_signage.id, name: location.web_signage.name}
+    end
+  end
   attribute :created_at do |location|
     location.created_at && location.created_at.strftime('%d/%m/%Y')
   end
