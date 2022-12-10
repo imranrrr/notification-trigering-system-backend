6092408a9a8b78ddb90d1952b731738990d21749
emran <emranlateef786@gmail.com>
2022-12-10 18:29:30 +0500

    fixed issue

--- db/migrate/20221210122129_add_creator_id_to_subscription.rb
+++
@@ -1,5 +0,0 @@
-class AddCreatorIdToSubscription < ActiveRecord::Migration[7.0]
-  def change
-    add_column :subscriptions, :creator_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_10_122129) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_10_055557) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -133,7 +133,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_10_122129) do
     t.datetime "start_date"
     t.datetime "end_date"
     t.bigint "company_id"
-    t.integer "creator_id"
     t.index ["company_id"], name: "index_subscriptions_on_company_id"
     t.index ["package_id"], name: "index_subscriptions_on_package_id"
     t.index ["status"], name: "index_subscriptions_on_status"
